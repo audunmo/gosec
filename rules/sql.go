73efb6de43a0f8318bd1fd73e16ad34d29faed1c
Audun Bjørnerud Mo <audun.mo@remarkable.no>
2023-07-14 14:23:12 +0200

    fix: remove some unecessary breakpoints

--- rules/sql.go
+++ rules/sql.go
@@ -237,13 +237,10 @@ func (s *sqlStrFormat) checkQuery(call *ast.CallExpr, ctx *gosec.Context) (*issu
 
 func (s *sqlStrFormat) checkFormatting(n ast.Node, ctx *gosec.Context) *issue.Issue {
 	// argIndex changes the function argument which gets matched to the regex
-	runtime.Breakpoint()
 	argIndex := 0
 	if node := s.fmtCalls.ContainsPkgCallExpr(n, ctx, false); node != nil {
-		runtime.Breakpoint()
 		// if the function is fmt.Fprintf, search for SQL statement in Args[1] instead
 		if sel, ok := node.Fun.(*ast.SelectorExpr); ok {
-			runtime.Breakpoint()
 			if sel.Sel.Name == "Fprintf" {
 				// if os.Stderr or os.Stdout is in Arg[0], mark as no issue
 				if arg, ok := node.Args[0].(*ast.SelectorExpr); ok {
@@ -259,14 +256,10 @@ func (s *sqlStrFormat) checkFormatting(n ast.Node, ctx *gosec.Context) *issue.Is
 			}
 		}
 
-		runtime.Breakpoint()
-
 		// no formatter
 		if len(node.Args) == 0 {
 			return nil
 		}
-
-		runtime.Breakpoint()
 
 		var formatter string
 
@@ -275,41 +268,30 @@ func (s *sqlStrFormat) checkFormatting(n ast.Node, ctx *gosec.Context) *issue.Is
 
 			runtime.Breakpoint()
 			if fullStr, ok := gosec.ConcatString(argExpr); ok {
-				runtime.Breakpoint()
 				formatter = fullStr
 			}
 		} else if arg, e := gosec.GetString(node.Args[argIndex]); e == nil {
-			runtime.Breakpoint()
 			formatter = arg
 		}
 		if len(formatter) <= 0 {
-			runtime.Breakpoint()
 			return nil
 		}
 
-		runtime.Breakpoint()
 		// If all formatter args are quoted or constant, then the SQL construction is safe
 		if argIndex < len(node.Args) {
-			runtime.Breakpoint()
 			allSafe := true
 			for _, arg := range node.Args[argIndex+1:] {
-				runtime.Breakpoint()
 				if n := s.noIssueQuoted.ContainsPkgCallExpr(arg, ctx, true); n == nil && !s.constObject(arg, ctx) {
-					runtime.Breakpoint()
 					allSafe = false
 					break
 				}
 			}
-			runtime.Breakpoint()
 			if allSafe {
-				runtime.Breakpoint()
 				return nil
 			}
 		}
 
-		runtime.Breakpoint()
 		if s.MatchPatterns(formatter) {
-			runtime.Breakpoint()
 			return ctx.NewIssue(n, s.ID(), s.What, s.Severity, s.Confidence)
 		}
 	}
